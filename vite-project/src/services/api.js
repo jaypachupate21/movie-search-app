913cda421a260234c3fc61f1f014d91b3edc0404
Jay Avinash Pachupate <pachupatejay2102@gmail.com>
2025-04-03 19:07:35 +0530

    again removing api key from history

--- vite-project/src/services/api.js
+++ vite-project/src/services/api.js
@@ -1,8 +1,4 @@
-
-require('dotenv').config();
-const apiKey=process.env.API_KEY;
-
-const API_KEY=apiKey;
+const API_KEY=import.meta.env.VITE_Api_Key;
 const BASE_URL="https://api.themoviedb.org/3";
 
 
