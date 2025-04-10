b5c330d0eca7c0b59fbcdf7d820f777420212934
Jay Avinash Pachupate <pachupatejay2102@gmail.com>
2025-04-10 19:25:01 +0530

    minor modification in all files

--- vite-project/src/components/NavBar.jsx
+++ vite-project/src/components/NavBar.jsx
@@ -1,9 +1,8 @@
- import {Link} from "react-router-dom"
- import "../css/Navbar.css"
+import { Link } from "react-router-dom";
+import "../css/Navbar.css"
 
-function NavBar(){
-    return
-    <nav className="navbar">
+function NavBar() {
+    return <nav className="navbar">
         <div className="navbar-brand">
             <Link to="/">Movie App</Link>
         </div>
--- vite-project/src/pages/Favorites.jsx
+++ vite-project/src/pages/Favorites.jsx
@@ -1,31 +1,29 @@
-import "../css/Favorites.css"
-import {useMovieContext} from "../contexts/MovieContext"
-import MovieCard from "../components/MovieCard"
+import "../css/Favorites.css";
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
 
+function Favorites() {
+  const { favorites } = useMovieContext();
 
-function Favorites(){
-
-    const {favorites}=useMovieContext();
-
-    if(favorites){
-        return 
-        <div className="favorites">
-
-            <h2>Your Favorites</h2>
-            <div className="movies-grid">
-                {favorites.map((movie)=> (<MovieCard movie={movie} key={movie.id}/>))}
-            </div>
+  if (favorites) {
+    return (
+      <div className="favorites">
+        <h2>Your Favorites</h2>
+        <div className="movies-grid">
+          {favorites.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
         </div>
+      </div>
+    );
+  }
 
-        
-    }
-
-
-    return <div className="favourites-empty">
-        <h2>No favorite yet</h2>
-        <p>Start adding movies to your favorites and they will appear here</p>
-
+  return (
+    <div className="favorites-empty">
+      <h2>No Favorite Movies Yet</h2>
+      <p>Start adding movies to your favorites and they will appear here!</p>
     </div>
+  );
 }
 
-export default Favorites
+export default Favorites;
